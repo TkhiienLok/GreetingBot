5a5ce2e0434e7851e1ed9c1712aff04976bef91f
TkhiienLok <tkhiien@gmail.com>
2019-06-02 02:44:05 +0300

    Added Flask app

--- run_app.py
+++ run_app.py
@@ -1,14 +1,22 @@
 import os
 from flask import Flask, render_template
+from greet_bot import main
 
 
-def create_app(data=default_data):
+def create_app():
     """Function to create and return an application"""
     app = Flask(__name__)
-    app.config['data'] = data
+
+    @app.route("/")
+    def run_bot():
+        main()
 
     return app
 
 # running application on heroku
 port = int(os.environ.get("PORT", 5000))
 create_app().run(debug=True, host='0.0.0.0', port=port)
+
+##local mashine
+##if __name__ == "__main__":
+##    create_app().run()
