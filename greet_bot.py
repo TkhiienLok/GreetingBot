74d057a16ccea4579b34ed33f1dcc6b864c500e8
TkhiienLok <tkhiien@gmail.com>
2019-06-02 01:08:17 +0300

    Improved greeting recognition

--- greet_bot.py
+++ greet_bot.py
@@ -53,21 +53,22 @@ en_alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
 greet_bot = BotHandler('716926010:AAExHWbqTeVcoaTvwN7qSqide8vLwN0Wd4s') 
 
-ru_greetings = ('здравствуй', 'привет', 'ку', 'здоров','здрасте','здравствуйте','куку', 'здр')  #Russian greeting words 
+ru_greetings = ('здравствуй', 'привет', 'ку', 'здоров','здрасте','куку', 'здр','здаров')  #Russian greeting words 
 en_greetings = ('hello', 'hi', 'hey', 'whatsup')  # English greeting words
 
 now = datetime.datetime.now()
 ru_bot_greetings = ('доброе утро', 'добрый день', 'добрый вечер')
 en_bot_greetings = ('good morning', 'good afternoon', 'good evening')
 
-# removing all punctuation signs
-def remove_punctuation(text):
-    remove = string.punctuation
-    remove = remove.replace("-", "") # don't remove hyphens
-    pattern = r"[{}]".format(remove) # create the pattern
-
-    text = re.sub(pattern, " ", text) # substitute punctuation mark with space (in case there was no space between words)
-    return text
+# removing all punctuation signs and offtopic words
+def remove_extra(text):
+    text = re.sub(r'what\'* *s *up','whatsup', text)  # substitute "what's up" written in any possible way to 'whatsup'
+    text = re.split(r'[{}]'.format(string.punctuation), text.lower())  # remove punctuation
+    text_words = ''.join(text).split()  # make a list of words
+    for word in text_words:
+        if re.match(r'\w*здорово*\w{1,2}\b', word): # delete such words as "выздоровел", "здоровый", "здоровая", etc, which meaning is about "health"
+            text_words.remove(word)
+    return text_words
     
 
 def main():  
@@ -88,19 +89,19 @@ def main():
 
         user_greeted = False
 
-        # all words from message
-        user_message_words = remove_punctuation(last_chat_text.lower()).split()
+        # list of all words from the message with no punctuation
+        user_message_words = remove_extra(last_chat_text.lower())
 
         # check if there is any Russion greeting word in message
         for phrase in all_rus_phrases:
-            if phrase in user_message_words or ((len(phrase) > 3) and phrase in last_chat_text.lower()):
+            if phrase in user_message_words or ((len(phrase) > 4) and phrase in last_chat_text.lower()):
                 bot_greetings = ru_bot_greetings
                 user_greeted = True
                 break
 
         # check if there is any English greeting word in message    
         for phrase in all_eng_phrases:
-            if phrase in user_message_words or ((len(phrase) > 3) and phrase in last_chat_text.lower()):
+            if phrase in user_message_words or ((len(phrase) > 4) and phrase in last_chat_text.lower()):
                 bot_greetings = en_bot_greetings
                 user_greeted = True
                 break                        
