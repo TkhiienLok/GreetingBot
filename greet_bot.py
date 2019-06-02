c66ed0f8f82a2af770ab631ebe11372e7398394c
TkhiienLok <tkhiien@gmail.com>
2019-06-02 12:21:14 +0300

    Added a time zone defining

--- greet_bot.py
+++ greet_bot.py
@@ -3,9 +3,7 @@ import datetime
 import re
 import string
 import datetime
-from time_zone_bot import Bot
-
-
+import pytz
 
 
 class BotHandler:
@@ -37,9 +35,10 @@ class BotHandler:
 
         return last_update
     
-    def greet_user(self,nowdata, chat_id, name, greetings,offset_time):
+    def greet_user(self,nowdata, chat_id, name, greetings,offset):
         today = nowdata.day
-        hour = nowdata.hour + offset_time
+        hour = nowdata.hour + offset
+        print('hour', hour)
 
         # check day time and send greeting
         if today == nowdata.day and 0 <= hour < 12:
@@ -61,7 +60,7 @@ greet_bot = BotHandler(TOKEN)
 ru_greetings = ('здравствуй', 'привет', 'ку', 'здоров','здрасте','куку', 'здр','здаров','кукусики')  #Russian greeting words 
 en_greetings = ('hello', 'hi', 'hey', 'whatsup')  # English greeting words
 
-now = datetime.datetime.now()
+now = datetime.datetime.utcnow()
 ru_bot_greetings = ('доброе утро', 'добрый день', 'добрый вечер')
 en_bot_greetings = ('good morning', 'good afternoon', 'good evening')
 
@@ -74,13 +73,75 @@ def remove_extra(text):
         if re.match(r'\w*здорово*\w{1,2}\b', word): # delete such words as "выздоровел", "здоровый", "здоровая", etc, which meaning is about "health"
             text_words.remove(word)
     return text_words
+
+
+def check_zone(zone_text):
+    tz = re.match(r'\w+ *-? *\w* *[/-]? *\w+ *[+-]? *\d*', zone_text)  # check format of user input
+    tz_parts = []  
+    if tz:
+        tz = re.split(r'[{}]'.format(string.punctuation), zone_text.lower())
+        tz_parts = ' '.join(tz).split()  # split user input to get just letters and digits
+    print(tz_parts)
+
+    time_zone_words = None
+    for tz in pytz.all_timezones:   # defining time zone from library
+        right_tz = True
+        for part in tz_parts:
+            if not (part in tz.lower()):
+                right_tz = False
+        if right_tz:
+            time_zone_words = tz
+    return time_zone_words
+
+
+def set_time_offset():
+    new_offset = None
+    greet_bot.get_updates(new_offset)
+    last_update = greet_bot.get_last_update()
+    last_update_id = last_update['update_id']
+    last_chat_text = last_update['message']['text']
+    last_chat_id = last_update['message']['chat']['id']
+    last_chat_name = last_update['message']['chat']['first_name']
+    last_few_words = last_chat_text[:15]
+
+    msg = "Hey, {}, before talking about \" {}...bla, bla, bla\", could we set up time first? I'd appriciate if you enter your time zone (you can use either spaces or dashes or slashes in your input)".format(last_chat_name, last_few_words)
+
+    greet_bot.send_message(last_chat_id, msg)
+    new_offset = last_update_id + 1
+
+    greet_bot.get_updates(new_offset)
+    last_update = greet_bot.get_last_update()
+    last_update_id = last_update['update_id']
+    last_chat_text = last_update['message']['text']
+    new_offset = last_update_id + 1
+    timezone = check_zone(last_chat_text)
+    print(timezone)
+    
+    while not timezone:
+        greet_bot.send_message(last_chat_id, 'Enter a valid time zone, please')
+        new_offset = last_update_id + 1
+
+        greet_bot.get_updates(new_offset)
+        last_update = greet_bot.get_last_update()
+        last_update_id = last_update['update_id']
+        last_chat_text = last_update['message']['text']
+        timezone = check_zone(last_chat_text)
+        new_offset = last_update_id + 1
+            
+    
+    est = pytz.timezone(timezone)
+    now = datetime.datetime.utcnow()
+    time_zone_offset = str(est.localize(now).utcoffset()).split(':')[0]
+
+    greet_bot.send_message(last_chat_id, 'Now, I am all set up, thanks')
+    return int(time_zone_offset)
     
 
 def main():  
     new_offset = None
-
-    a = Bot(TOKEN)
-    offcet = a.run()
+    time_offset = set_time_offset()
+    print('time offset', time_offset)
+    
 
     while True:
         greet_bot.get_updates(new_offset)
@@ -117,16 +178,16 @@ def main():
         # if user sent message with no greeting            
         if not user_greeted:
             if last_chat_text.lower()[0] in ru_alphabet:
-                greet_bot.send_message(last_chat_id, 'Я умею только здороваться')
+                greet_bot.send_message(last_chat_id, 'Я умею только здороваться на русском и английском')
             elif last_chat_text.lower()[0] in en_alphabet:
-                greet_bot.send_message(last_chat_id, 'The only thing I can do is to greet people')
+                greet_bot.send_message(last_chat_id, 'The only thing I can do is to greet people in Russian and English')
             else:
                 greet_bot.send_message(last_chat_id, 'For now, I only understand English and Russian')
             new_offset = last_update_id + 1
             continue
 
         # greet user
-        greet_bot.greet_user(now, last_chat_id, last_chat_name, bot_greetings,offcet)
+        greet_bot.greet_user(now, last_chat_id, last_chat_name, bot_greetings,time_offset)
         new_offset = last_update_id + 1
 
 if __name__ == '__main__':  
